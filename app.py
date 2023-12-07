5c68a2fd9bc61277aa24dc7527a3b20a58bf535c
greg215 <greg.hu@wolt.com>
2023-12-07 19:33:52 +0100

    blank line

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def calculate():
     else:
         result = 'Invalid operation'
 
-
     return render_template('index.html', result=result)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
